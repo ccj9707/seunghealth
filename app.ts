c338efa59a2bbd90db7c2d897f091697943ef3db
kay <kay@yellotravel.com>
2018-04-23 01:05:04 +0900

    Add app

---
+++ app.ts
@@ -0,0 +1,18 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
+app.use(express.static("workspace"));
+
+app.use((err: Error, req, res, next) => {
+  res.json({
+    status: 500,
+    message: err.message
+  });
+});
+
+export { app };
